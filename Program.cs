ce10d5f4be60cbe03718f8c6e83900286dfdf0a2
shamiconti <shamiconti15@gmail.com>
2025-05-06 18:48:51 -0300

    Actualización del mensaje de bienvenida

--- Program.cs
+++ Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("¡Lindo viernes para estar con Visual Studio 2022!");
+Console.WriteLine("¡Primer cambio en el código, subiendo a GitHub!");
